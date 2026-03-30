24e8000008c4924ce49b5a8595f6570a5789d86b
towqeeraltaf777 <Towqeer.Altaf@sysusa.com>
2026-03-30 16:49:19 +0530

    Create csdmII.jsx
    csdmII

---
+++ src/data/csdmII.jsx
@@ -0,0 +1,39 @@
+const CSDMII = [
+{
+  id: 1,
+  text: "From the Get Well Playbook 'Handle CMDB tables begin with u_cmdb_ci', what are upstream causes of having custom tables in the CMDB?",
+  options: [
+    "No review process in creation of CI classes",
+    "Highly skilled ServiceNow developers",
+    "Requests from stakeholders",
+    "No governance process for CMDB customization"
+  ],
+  correctAnswers: [
+    "No review process in creation of CI classes",
+    "No governance process for CMDB customization"
+  ],
+  multipleChoice: true
+},
+{
+  id: 2,
+  text: "Which are valid Data Manager policy types?",
+  options: [
+    "Retirement",
+    "Attestation",
+    "Archiving",
+    "Deletion",
+    "De-duplication",
+    "Certification"
+  ],
+  correctAnswers: [
+    "Retirement",
+    "Attestation",
+    "Archiving",
+    "Deletion",
+    "Certification"
+  ],
+  multipleChoice: true
+},
+];
+
+export default CSDMII;
